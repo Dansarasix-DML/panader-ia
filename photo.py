2196497ac3602d06dc0465d9ae83521c027ba344
ccarluq0911 <ccarluq0911@g.educaand.es>
2025-03-04 18:56:42 +0100

    Cambiar que haga fotos distintas

--- photo.py
+++ photo.py
@@ -12,32 +12,34 @@ capturando = False  # Variable de control
 
 def capturar_fotos_automaticas(socketio, interval=5):
     global capturando
-    cap = cv2.VideoCapture(0)  # Iniciar la cámara
-
-    if not cap.isOpened():
-        print("No se pudo abrir la camara")
-        return
-
     capturando = True  # Activar captura
 
     try:
         while capturando:
+            cap = cv2.VideoCapture(0)  # Iniciar la cámara
+
+            if not cap.isOpened():
+                print("No se pudo abrir la camara")
+                return
+            
             ret, frame = cap.read()
             if not ret:
                 print("Error al capturar imagen")
                 break
 
-            timestamp = int(time.time())
-            image_path = os.path.join(IMAGE_FOLDER, f"captura_{timestamp}.jpg")
+            counter = 0
+            image_path = os.path.join(IMAGE_FOLDER, f"captura_{counter}.jpg")
 
             cv2.imwrite(image_path, frame)  # Guardar imagen
 
             with open(image_path, "rb") as img_file:
                 img_bytes = img_file.read()
                 socketio.emit("nueva_imagen", img_bytes)  # Enviar imagen al frontend
-
-            time.sleep(int(interval) * 60)  # Esperar 2 segundos antes de la siguiente captura
+ 
+            cap.release()
+            print("Camara liberada")
+            counter = counter + 1
+            time.sleep(int(interval) * 10)  # Esperar 2 segundos antes de la siguiente captura
 
     finally:
-        cap.release()
-        print("Camara liberada")
+        capturando = False
