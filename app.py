5d01ae33a2af05a921b0ef8a09d268a0451dda83
carloschaves12 <carloschaveshernandez2@gmail.com>
2025-03-11 17:17:06 +0100

    CORS

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from photo import capturar_fotos_automaticas
-from flask_cors import cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-#CORS(app)
-#CORS(app, resources={r"/*": {"origins": "http://192.168.127.138:5002"}})
+CORS(app, resources={r"/*": {"origins": "http://192.168.127.138:5002"}})
 
 capturando = False
 
 @app.route('/')
-@cross_origin(origins="http://192.168.127.138/")
+
 def index():
     return render_template("index.html")
 
