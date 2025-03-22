03a918ab48ef0d1aa831ad2485f887a249a93650
PabloE-03 <peibol.max@gmail.com>
2025-03-22 20:27:29 +0100

    solucion ante el time.sleep

--- photo.py
+++ photo.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 import time
-from datetime import datetime
+from datetime import datetime,timedelta
 import logging
 
 logging.basicConfig(
@@ -32,30 +32,41 @@ class Capturadora:
         if not cap.isOpened():
             print("No se pudo abrir la cámara")
             return
+
+        date_now = datetime.now()
+        date_str = date_now.strftime("%H:%M:%S")
+        date_next = date_str
         
         try:
             while self.capturando:
-                if not cap.isOpened():
-                    logging.info("Preparando camara para realizar una captura")
-                    cap = cv2.VideoCapture(0)
-                ret, frame = cap.read()
-                if not ret:
-                    print("Error al capturar imagen")
-                    break
-                fecha = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
-                image_path = os.path.join(self.image_folder, f"captura_{fecha}.png")
-                frame = frame[::-1] # invertir la imagen
-                cv2.imwrite(image_path, frame)  # Guardar imagen
-                with open(image_path, "rb") as img_file:
-                    img_bytes = img_file.read()
-                    if self.first_img is None:
-                        self.first_img = img_bytes
-                    self.img_now = img_bytes
-                    self.imgs += 1
-                    logging.info(f"Nueva imagen generada, nombre del archivo captura_{fecha}.png")
-                cap.release()
-                logging.info("Camara apagada esperando a la siguiente captura")
-                time.sleep(int(self.interval) * 60)  # Esperar 2 segundos antes de la siguiente captura
+                if(date_str==date_next):
+                    if not cap.isOpened():
+                        logging.info("Preparando camara para realizar una captura")
+                        cap = cv2.VideoCapture(0)
+                    ret, frame = cap.read()
+                    if not ret:
+                        print("Error al capturar imagen")
+                        break
+                    fecha = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+                    image_path = os.path.join(self.image_folder, f"captura_{fecha}.png")
+                    frame = frame[::-1] # invertir la imagen
+                    cv2.imwrite(image_path, frame)  # Guardar imagen
+                    with open(image_path, "rb") as img_file:
+                        img_bytes = img_file.read()
+                        if self.first_img is None:
+                            self.first_img = img_bytes
+                        self.img_now = img_bytes
+                        self.imgs += 1
+                        logging.info(f"Nueva imagen generada, nombre del archivo captura_{fecha}.png")
+                    cap.release()
+                    logging.info("Camara apagada esperando a la siguiente captura")
+                    # time.sleep(int(self.interval) * 60)  # Esperar 2 segundos antes de la siguiente captura
+                    date_next = date_now + timedelta(minutes=self.interval)
+                    date_next = date_next.strftime("%H:%M:%S")
+                    logging.info(f"La siguiente captura se realizara en la siguiente hora {date_next}")
+                date_now = datetime.now()
+                date_str = date_now.strftime("%H:%M:%S")
+
         finally:
             logging.info("Camara detenida, el servidor esta preparado para iniciar una nueva captura")
             cap.release()
